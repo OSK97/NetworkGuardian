9f91608bdaf63ebeea2baa143c642227be70b8ef
os <os@YP5CPJIA>
2026-07-04 13:41:42 +0530

    Add project files.

---
+++ core/NetworkMoniter.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.NetworkInformation;
+
+namespace NetworkGuardian.Core
+{
+    public class NetworkMonitor
+    {
+        public void PrintAllAdapters()
+        {
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+
+            foreach (NetworkInterface adapter in adapters)
+            {
+                System.Diagnostics.Debug.WriteLine(adapter.Name);
+            }
+        }
+    }
+}
+
---
+++ models/NetworkState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkGuardian.Models
+{
+   
+}
+
+public class NetworkState
+{
+    public bool TpConnected { get; set; }
+    public bool InternetAvailable { get; set; }
+    public bool HotspotRunning { get; set; }
+    public bool RecoveryInProgress { get; set; }
+}
